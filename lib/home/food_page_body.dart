1cc634856b71fa09239d68248a27baac1cb6dadb
zaidoun92 <27ew75ma@gmail.com>
2022-08-10 15:17:32 +0300

    Complete ui horizontal list view under AppBar

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text_widget.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,8 +13,102 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 320,
+      child: PageView.builder(
+          controller: pageController,
+          itemCount: 5,
+          itemBuilder: (context, position) {
+            return _buildPageItem(position);
+          }),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven
+                ? const Color(0xFF69c5df)
+                : const Color(0xFF9294cc),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                "assets/image/food0.png",
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Chinese Side"),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => const Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "1287"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: const [
+                      IconAndTextWidget(
+                        icon: Icons.circle_sharp,
+                        text: "Normal",
+                        iconColor: AppColors.iconColor1,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.location_on,
+                        text: "1.7km",
+                        iconColor: AppColors.mainColor,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.access_time_filled_rounded,
+                        text: "32min",
+                        iconColor: AppColors.iconColor2,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
@@ -52,6 +53,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
               ),
             ),
           ),
+          FoodPageBody(),
         ],
       ),
     );
