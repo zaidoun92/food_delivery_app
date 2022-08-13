1065f9bc6d2a32270c77c3d58fa59d40cda1be39
zaidoun92 <27ew75ma@gmail.com>
2022-08-13 14:18:34 +0300

    Add The Shadow to the Box

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -115,6 +115,21 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5),
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, 0),
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(5, 0),
+                  ),
+                ],
               ),
               child: Container(
                 padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
