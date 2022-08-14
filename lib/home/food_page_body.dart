cb5a552afa32b4170518375c46fe450e77ac105f
zaidoun92 <27ew75ma@gmail.com>
2022-08-14 11:41:03 +0300

    Complete the Popular text

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -40,6 +40,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Slider Section
         Container(
           height: Dimensions.pageView,
           child: PageView.builder(
@@ -49,6 +50,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                 return _buildPageItem(position);
               }),
         ),
+        // dots
         DotsIndicator(
           dotsCount: 5,
           position: _currPageValue,
@@ -58,6 +60,27 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             activeSize: const Size(18.0, 9.0),
             activeShape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(5.0)),
+          ),
+        ),
+        //popular text
+        SizedBox(height: Dimensions.height30),
+        Container(
+          margin: EdgeInsets.only(left: Dimensions.width30),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              BigText(text: "Popular"),
+              SizedBox(width: Dimensions.width10),
+              Container(
+                margin: const EdgeInsets.only(bottom: 3),
+                child: BigText(text: ".", color: Colors.black26),
+              ),
+              SizedBox(width: Dimensions.width10),
+              Container(
+                margin: const EdgeInsets.only(bottom: 2),
+                child: SmallText(text: "Food pairing"),
+              ),
+            ],
           ),
         ),
       ],
