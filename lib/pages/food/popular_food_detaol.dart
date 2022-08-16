97ee03747f4839fa5a86ba0192fa283e6317dd52
zaidoun92 <27ew75ma@gmail.com>
2022-08-16 13:55:49 +0300

    complete image background and icons in the header

---
+++ lib/pages/food/popular_food_detaol.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/dimansions.dart';
+import 'package:food_delivery/widgets/app_icon.dart';
+
+class PopularFoodDetail extends StatelessWidget {
+  const PopularFoodDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.maxFinite,
+              height: Dimensions.popularFoodImgSize,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    "assets/image/food0.png",
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
